3981b5612f62dfc07542d2bac4b4100c313830e5
bsord <brandon.sorgdrager@gmail.com>
2020-11-02 14:42:15 -0800

    feat(workflow): add tf and workflow core

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  backend "remote" {
+    organization = "Fairbanks-io"
+
+    workspaces {
+      name = "tf-iac-demo"
+    }
+  }
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 1.22.2"
+    }
+    cloudflare = {
+      # Source is required for providers in other namespaces, to avoid ambiguity.
+      source  = "cloudflare/cloudflare"
+      version = "~> 2.11.0"
+    }
+    kubernetes = {
+      # Source is required for providers in other namespaces, to avoid ambiguity.
+      source  = "hashicorp/kubernetes"
+      version = "~> 1.13.2"
+    }
+    helm = {
+      # Source is required for providers in other namespaces, to avoid ambiguity.
+      source  = "hashicorp/helm"
+      version = "~> 1.3.0"
+    }
+  }
+}
