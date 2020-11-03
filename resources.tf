c30fd2c45a4c7a57b536573d45d0c0ae189762e3
bsord <brandon.sorgdrager@gmail.com>
2020-11-02 16:02:56 -0800

    fix(cluster): change droplet size

--- resources.tf
+++ resources.tf
@@ -7,7 +7,7 @@ resource "digitalocean_kubernetes_cluster" "k8s" {
 
   node_pool {
     name       = "worker-pool"
-    size       = "s-1vcpu-1gb"
+    size       = "s-1vcpu-2gb"
     node_count = 3
   }
 }
